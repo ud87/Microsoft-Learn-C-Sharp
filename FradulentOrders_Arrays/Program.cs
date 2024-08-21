e2c65a0e21a65be4680e90714dbf41990f274809
Udaya <udayarai@hotmail.co.uk>
2024-08-21 22:54:39 +0100

    Initial commit

---
+++ FradulentOrders_Arrays/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+//Declare a new array
+string[] fradulentOrderIDs = new string[3]; //creates a new instance of an array in pc memory that can hold 3 string values
+
+//Assign values to elements of an array
+fradulentOrderIDs[0] = "A213";
+fradulentOrderIDs[1] = "B456";
+fradulentOrderIDs[2] = "C789";
+//fradulentOrderIDs[3] = "D000";
+*/
+
+string[] fradulentOrderIDs = ["A213", "B456", "C789"];      //collection expression syntax can also use {} instead of []
+
+Console.WriteLine($"First: {fradulentOrderIDs[0]}");
+Console.WriteLine($"Second: {fradulentOrderIDs[1]}");
+Console.WriteLine($"Third: {fradulentOrderIDs[2]}");
+
+fradulentOrderIDs[0] = "F000";
+Console.WriteLine($"Reassign First: {fradulentOrderIDs[0]}");
+
+//length of an array can be found using .Length property
+Console.WriteLine($"There are {fradulentOrderIDs.Length} fradulent orders to process.");
