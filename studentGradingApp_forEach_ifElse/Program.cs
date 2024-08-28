b4555e2ff13a1816a2a6c430868a5178c58f33b8
Udaya <udayarai@hotmail.co.uk>
2024-08-28 07:17:25 +0100

    for loop and if/else feature added to calculate grades logically

--- studentGradingApp_forEach_ifElse/Program.cs
+++ studentGradingApp_forEach_ifElse/Program.cs
@@ -1,30 +1,96 @@
-﻿using System;
-using System.IO;
-
-//initialize variables - graded assignments
+﻿//initialize variables - graded assignments
 int currentAssignments = 5;
-int[] sophiaScores = new int[] {90, 86, 87, 98, 100};
-int[] andrewScores = new int[] {92, 89, 81, 96, 90};
-int[] emmaScores = new int[] {90, 85, 87, 98, 68};
-int[] loganScores = new int[] {90, 95, 87, 88, 96};
+int[] sophiaScores = new int[] { 90, 86, 87, 98, 100 };
+int[] andrewScores = new int[] { 92, 89, 81, 96, 90 };
+int[] emmaScores = new int[] { 90, 85, 87, 98, 68 };
+int[] loganScores = new int[] { 90, 95, 87, 88, 96 };
 
+//student names
+string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
+int[] studentScores = new int[10];
 
-int sophiaSum = 0;
+//Write the Report Header to the console
+Console.WriteLine("Student\t\tGrade\n");
 
-decimal sophiaScore;
-
-foreach (int score in sophiaScores)
+foreach (string name in studentNames)
 {
-    //add the exam score to the sum
-    sophiaSum += score;
+    string currentStudent = name;
+
+    if (currentStudent == "Sophia")
+        studentScores = sophiaScores;
+
+    else if (currentStudent == "Andrew")
+        studentScores = andrewScores;
+
+    else if (currentStudent == "Emma")
+        studentScores = emmaScores;
+
+    else if (currentStudent == "Logan")
+        studentScores = loganScores;
+
+    
+
+    int sumAssignmentScores = 0;
+
+    decimal currentStudentGrade;
+
+    string currentStudentLetterGrade = "";
+
+    foreach (int score in studentScores)
+    {
+        //add the exam score to the sum
+        sumAssignmentScores += score;
+    }
+
+    currentStudentGrade = (decimal)sumAssignmentScores / currentAssignments;
+
+    if (currentStudentGrade >= 97)
+        currentStudentLetterGrade = "A+";
+
+    else if (currentStudentGrade >= 93)
+        currentStudentLetterGrade = "A";
+
+    else if (currentStudentGrade >= 90)
+        currentStudentLetterGrade = "A-";
+
+    else if (currentStudentGrade >= 87)
+        currentStudentLetterGrade = "B+";
+
+    else if (currentStudentGrade >= 83)
+        currentStudentLetterGrade = "B";
+
+    else if (currentStudentGrade >= 80)
+        currentStudentLetterGrade = "B-";
+
+    else if (currentStudentGrade >= 77)
+        currentStudentLetterGrade = "C+";
+
+    else if (currentStudentGrade >= 73)
+        currentStudentLetterGrade = "C";
+
+    else if (currentStudentGrade >= 70)
+        currentStudentLetterGrade = "C-";
+
+    else if (currentStudentGrade >= 67)
+        currentStudentLetterGrade = "D+";
+
+    else if (currentStudentGrade >= 63)
+        currentStudentLetterGrade = "D";
+
+    else if (currentStudentGrade >= 60)
+        currentStudentLetterGrade = "D-";
+    
+    else
+        currentStudentLetterGrade = "F";
+    
+
+    Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}");
 }
 
 
-sophiaScore = (decimal)sophiaSum / currentAssignments;
 
 
-Console.WriteLine("Student\t\tGrade\n");
-Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
+
 
 Console.WriteLine("Press the Enter key to continue");
 Console.ReadLine();   //pauses the application so that the application user can review the output
--- studentGradingApp_forEach_ifElse/obj/Debug/net8.0/studentGradingApp_forEach_ifElse.AssemblyInfo.cs
+++ studentGradingApp_forEach_ifElse/obj/Debug/net8.0/studentGradingApp_forEach_ifElse.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("studentGradingApp_forEach_ifElse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ed81ec65014cb775bb52be7b6da2efa52fe07b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e82d91f6bdf1f2a255d0f0ffafc2d65ffdc6d4d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("studentGradingApp_forEach_ifElse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("studentGradingApp_forEach_ifElse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
